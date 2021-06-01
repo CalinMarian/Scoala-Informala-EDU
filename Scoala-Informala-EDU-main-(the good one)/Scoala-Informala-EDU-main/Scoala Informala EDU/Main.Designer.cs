2a86ced7ca9aab9c1b6ebc941ed79f267b6a8a81
DSzocs <84030178+DSzocs@users.noreply.github.com>
2021-06-01 19:52:19 +0300

    Add files via upload

---
+++ Scoala-Informala-EDU-main-(the good one)/Scoala-Informala-EDU-main/Scoala Informala EDU/Main.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Scoala_Informala_EDU
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLearn = new System.Windows.Forms.Button();
+            this.btnQuiz = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.RadioBtnMusic = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnLearn
+            // 
+            this.btnLearn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLearn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLearn.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.btnLearn.FlatAppearance.BorderSize = 2;
+            this.btnLearn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLearn.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnLearn.Location = new System.Drawing.Point(36, 210);
+            this.btnLearn.Name = "btnLearn";
+            this.btnLearn.Size = new System.Drawing.Size(194, 43);
+            this.btnLearn.TabIndex = 0;
+            this.btnLearn.Text = "Learn C#";
+            this.btnLearn.UseVisualStyleBackColor = true;
+            this.btnLearn.Click += new System.EventHandler(this.LearnClickEvent);
+            // 
+            // btnQuiz
+            // 
+            this.btnQuiz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnQuiz.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnQuiz.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.btnQuiz.FlatAppearance.BorderSize = 2;
+            this.btnQuiz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuiz.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnQuiz.Location = new System.Drawing.Point(36, 273);
+            this.btnQuiz.Name = "btnQuiz";
+            this.btnQuiz.Size = new System.Drawing.Size(194, 43);
+            this.btnQuiz.TabIndex = 1;
+            this.btnQuiz.Text = "Quiz";
+            this.btnQuiz.UseVisualStyleBackColor = true;
+            this.btnQuiz.Click += new System.EventHandler(this.QuizClickEvent);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbout.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.btnAbout.FlatAppearance.BorderSize = 2;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnAbout.Location = new System.Drawing.Point(36, 395);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(194, 43);
+            this.btnAbout.TabIndex = 3;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.AboutClickEvent);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.button3.FlatAppearance.BorderSize = 2;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(36, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Job Interview Tips";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.JobClickEvent);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnQuit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnQuit.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.btnQuit.FlatAppearance.BorderSize = 2;
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuit.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnQuit.Location = new System.Drawing.Point(36, 453);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(194, 43);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.QuitClickEvent);
+            // 
+            // RadioBtnMusic
+            // 
+            this.RadioBtnMusic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RadioBtnMusic.AutoSize = true;
+            this.RadioBtnMusic.Location = new System.Drawing.Point(860, 12);
+            this.RadioBtnMusic.Name = "RadioBtnMusic";
+            this.RadioBtnMusic.Size = new System.Drawing.Size(112, 19);
+            this.RadioBtnMusic.TabIndex = 9;
+            this.RadioBtnMusic.Text = "Play/Stop Music";
+            this.RadioBtnMusic.UseVisualStyleBackColor = true;
+            this.RadioBtnMusic.CheckedChanged += new System.EventHandler(this.btnRadioEvent);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BackgroundImage = global::Scoala_Informala_EDU.Properties.Resources.MainBackround;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 521);
+            this.Controls.Add(this.RadioBtnMusic);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnQuiz);
+            this.Controls.Add(this.btnLearn);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scoala Informala EDU";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLearn;
+        private System.Windows.Forms.Button btnQuiz;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.CheckBox RadioBtnMusic;
+    }
+}
+
---
+++ Scoala-Informala-EDU-main-(the good one)/Scoala-Informala-EDU-main/Scoala Informala EDU/Quiz.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scoala_Informala_EDU
+{
+    public partial class Quiz : Form
+    {
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+        
+
+        public Quiz()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+            
+
+            totalQuestions = 10;
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+            
+
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+               
+            }
+           
+            if(questionNumber == totalQuestions)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + score + " questions correctly " + Environment.NewLine +
+                    "Click OK to play again"
+                    );
+                
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+            
+        }
+
+        private void askQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "When was C# invented?";
+
+                    button1.Text = "1983";
+                    button2.Text = "2002";
+                    button3.Text = "2000";
+                    button4.Text = "2012";
+
+                    correctAnswer = 3;
+
+                    break;
+                case 2:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of the following is an 8-byte integer";
+
+                    button1.Text = "Char";
+                    button2.Text = "Long";
+                    button3.Text = "Short";
+                    button4.Text = "Integer";
+
+                    correctAnswer = 2;
+
+                    break;
+                case 3:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of the following is the correct default value of a Boolean type?";
+
+                    button1.Text = "0";
+                    button2.Text = "1";
+                    button3.Text = "True";
+                    button4.Text = "False";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 4:
+                    pictureBox.Image = Properties.Resources.Q4;
+
+                    lblQuestion.Text = "Which of the following will be the correct output for the C#.NET code snippet given below?";
+
+                    button1.Text = "NagpuMumbair";
+                    button2.Text = "Nagpur Mumbai";
+                    button3.Text = "Mumbai";
+                    button4.Text = "NagpurMumbai";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 5:
+                    pictureBox.Image = Properties.Resources.Q5;
+
+
+                    lblQuestion.Text = "Which of the following will be the correct output for the C#.NET code snippet given below?";
+
+                    button1.Text = "ARE";
+                    button2.Text = "CRE";
+                    button3.Text = "CR";
+                    button4.Text = "CREATED";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 6:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of the following statements about the array are NOT true";
+
+                    button1.Text = "A one-dimensional array can store data in the form of table with many rows and columns";
+                    button2.Text = "To use an array, previously it is declared and defined";
+                    button3.Text = "The position of every element of the array is used to access that element that position";
+                    button4.Text = "A one-dimensional array can store different types of data";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 7:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of the following statement is correct about a string in C#.NET??";
+
+                    button1.Text = "A number cannot be represented in the form of a string";
+                    button2.Text = "The System.Array class is used to represent a string";
+                    button3.Text = " A string is mutable because it can be modified once it has been created";
+                    button4.Text = "A string has a zero-based index";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 8:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "The Method use to remove white space from a string?";
+
+                    button1.Text = "Remove()";
+                    button2.Text = "Split()";
+                    button3.Text = "Copy()";
+                    button4.Text = "Trim";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 9:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of these operators can be used to concatenate two or more String objects?";
+
+                    button1.Text = "/";
+                    button2.Text = "*";
+                    button3.Text = "+";
+                    button4.Text = "&&";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 10:
+                    pictureBox.Image = Properties.Resources.MainBackround;
+
+                    lblQuestion.Text = "Which of the following statements is correct";
+
+                    button1.Text = "A struct never declares a default constructor";
+                    button2.Text = "All value types in C# inherently derive from ValueType, which inherits from Object.";
+                    button3.Text = "A struct never declares a default destructor.";
+                    button4.Text = "In C#, classes and structs are semantically same";
+
+                    correctAnswer = 4;
+
+                    break;
+            }
+         
+        }
+
+       
+    }
+}
